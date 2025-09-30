ec17fb969696a630b1777092ccc5a81ee79c1398
Ye Yu <nearjilt@gmail.com>
2025-10-01 07:36:05 +0800

    fix: alias @react-native-async-storage/async-storage to false for web build

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ import type { NextConfig } from 'next';
 const config: NextConfig = {
   webpack: config => {
     config.externals.push('pino-pretty');
+    config.resolve.alias['@react-native-async-storage/async-storage'] = false;
     return config;
   },
 };
